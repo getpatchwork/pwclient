c2ac16cbb4920f9d697c210e6624f1e497a06e64
Stephen Finucane <stephen@that.guru>
2018-10-17 19:21:51 +0100

    Replace invalid use of parser failure
    This isn't a parsing issue - it's a config issue.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- pwclient/shell.py
+++ pwclient/shell.py
@@ -632,8 +632,9 @@ installed locales.
         try:
             project_str = config.get('options', 'default')
         except (ConfigParser.NoSectionError, ConfigParser.NoOptionError):
-            action_parser.error(
-                "No default project configured in %s\n" % CONFIG_FILE)
+            sys.stderr.write(
+                'No default project configured in %s\n' % CONFIG_FILE)
+            sys.exit(1)
 
     if not config.has_section(project_str):
         sys.stderr.write(
