61d712cee3edf6d8273fc82acc921c0ddbf711cf
Stephen Finucane <stephen@that.guru>
2019-09-06 22:19:51 +0100

    utils: Use SPDX license header
    Not sure how this was missed.
    
    Signed-off-by: Stephen Finucane <stephen@that.guru>

--- pwclient/utils.py
+++ pwclient/utils.py
@@ -1,20 +1,8 @@
-# Copyright (C) 2008 Nate Case <ncase@xes-inc.com>
+# Patchwork command line client
 # Copyright (C) 2018 Stephen Finucane <stephen@that.guru>
+# Copyright (C) 2008 Nate Case <ncase@xes-inc.com>
 #
-# This file is part of pwclient.
-#
-# pwclient is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# pwclient is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with pwclient; if not, see <http://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 import shutil
 import sys
